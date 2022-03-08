74d38cd68470b526b48609ec6816a59eca73c12d
Andreas Paepcke <paepcke@cs.stanford.edu>
2022-03-07 16:41:25 -0800

    Updated requirements and fixed up torchInference.py

--- src/inference/torchInference.py
+++ src/inference/torchInference.py
@@ -1,15 +1,15 @@
+#!/usr/bin/env python
+import os
 import torch
-import matplotlib.pyplot as plt
 import cv2
-import pandas as pd
 import time
 import argparse
 from collections import OrderedDict
+from pathlib import Path
 
+import matplotlib.pyplot as plt
 from training.model import createDeepLabv3
-
-print(torch.__version__)
-
+from torch.nn.functional import softmax
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -30,9 +30,9 @@ if torch.cuda.is_available():
 else:
     device = torch.device('cpu')
 
-model_info = torch.load(model_path)
+model_info = torch.load(model_path,map_location=device)
 if type(model_info) == OrderedDict:
-    model = createDeepLabv3()
+    model = createDeepLabv3(outputchannels=3)
     model.load_state_dict(model_info)
 else: 
     model = model_info
@@ -62,9 +62,30 @@ img = cv2.resize(originalImage, (256, 256), cv2.INTER_AREA).transpose(2,0,1)
 img = img.reshape(1, 3, img.shape[1],img.shape[2])
 
 start_time = time.time()
+# Get a dict:
+#    'out'  : mask-colored image [1,3,256,256]
+#    'aux'  : logits [1, 21, 256, 256]
 with torch.no_grad():
     if torch.cuda.is_available():
-    	a = model(torch.from_numpy(img).to(device).type(torch.cuda.FloatTensor)/255)
+        a = model(torch.from_numpy(img).to(device).type(torch.cuda.FloatTensor)/255)
     else:
         a = model(torch.from_numpy(img).to(device).type(torch.FloatTensor)/255)
 print("--- %s seconds ---" % (time.time() - start_time))
+
+# Get the image as [3, 256, 256]:
+outImage = a['out'].cpu().detach().squeeze().numpy()
+# Transposet to [256, 256, 3]:
+ax = plt.imshow(outImage.transpose([1,2,0]))
+
+# Create an output file name for the colored image:
+fstem   = Path(image_path).stem
+fsuffix = Path(image_path).suffix
+out_path = os.path.join('/tmp/', 
+                        f"{fstem}_masked_img{fsuffix}")
+print(f"Saving masked image to {out_path}")
+ax.figure.savefig(out_path)
+ax.figure.show()
+key = input("Press any key to quit and close the image: ")
+
+    
+
